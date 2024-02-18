cc4037e7634ce9f2be88f60da2d1a7b051c319fb
mfhollif <mileshollifieldgfp@gmail.com>
2024-02-18 11:52:32 -0500

    [Implementation] Implemented core game loop

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -6,57 +6,113 @@ using UnityEngine.UI;
 
 public class Quiz : MonoBehaviour
 {
+    [Header("Questions")]
     [SerializeField] TextMeshProUGUI questionText;
-    [SerializeField] QuestionSO question;
+    [SerializeField] List<QuestionSO> questions = new List<QuestionSO>();
+    QuestionSO currentQuestion;
+
+    [Header("Answers")]
     [SerializeField] GameObject[] answerButtons;
     int correctAnswerIndex;
+    bool hasAnsweredEarly;
+
+    [Header("Button Colors")]
     [SerializeField] Sprite defaultAnswerSprite;
     [SerializeField] Sprite correctAnswerSprite;
 
+    [Header("Timer")]
+    [SerializeField] Image timerImage;
+    Timer timer;
+
     void Start()
     {
-        // DisplayQuestion();
-        GetNextQuestion();
+        timer = FindObjectOfType<Timer>();
+    }
+
+    void Update()
+    {
+        timer.UpdateTimer();
+        timerImage.fillAmount = timer.fillFraction;
+        if (timer.loadNextQuestion)
+        {
+            hasAnsweredEarly = false;
+            GetNextQuestion();
+            timer.loadNextQuestion = false;
+        }
+        else if (!hasAnsweredEarly && !timer.isAnsweringQuestion)
+        {
+            DisplayAnswer(-1);
+            SetButtonState(false);
+        }
     }
 
     public void OnAnswerSelected(int index)
     {
+        hasAnsweredEarly = true;
+        DisplayAnswer(index);
+        SetButtonState(false);
+        timer.CancelTimer();
+    }
+
+    void DisplayAnswer(int index)
+    {
         Image buttonImage;
 
-        if(index == question.GetCorrectAnswerIndex()) {
+        if (index == currentQuestion.GetCorrectAnswerIndex())
+        {
             questionText.text = "Correct!";
             buttonImage = answerButtons[index].GetComponent<Image>();
             buttonImage.sprite = correctAnswerSprite;
-        } else {
-            correctAnswerIndex = question.GetCorrectAnswerIndex();
-            string correctAnswer = question.GetAnswer(correctAnswerIndex);
-            questionText.text = "Sorry, the correct answer was:\n" + correctAnswer;
+        }
+        else
+        {
+            correctAnswerIndex = currentQuestion.GetCorrectAnswerIndex();
+            string correctAnswer = currentQuestion.GetAnswer(correctAnswerIndex);
+            questionText.text = "Sorry, the correct answer was;\n" + correctAnswer;
             buttonImage = answerButtons[correctAnswerIndex].GetComponent<Image>();
             buttonImage.sprite = correctAnswerSprite;
         }
-        SetButtonState(false);
     }
 
     void GetNextQuestion()
     {
-        SetButtonState(true);
-        SetDefaultButtonSprites();
-        DisplayQuestion();
+        if (questions.Count > 0)
+        {
+            SetButtonState(true);
+            SetDefaultButtonSprites();
+            GetRandomQuestion();
+            DisplayQuestion();
+        }
+    }
+
+    void GetRandomQuestion()
+    {
+        int index = Random.Range(0, questions.Count);
+        currentQuestion = questions[index];
+
+        if (questions.Contains(currentQuestion))
+        {
+            questions.Remove(currentQuestion);
+        }
     }
 
     void DisplayQuestion()
     {
-        questionText.text = question.GetQuestion();
+        questionText.text = currentQuestion.GetQuestion();
 
-        for( int i = 0; i < answerButtons.Length; i++) {
+        for (int i = 0; i < answerButtons.Length; i++)
+        {
             TextMeshProUGUI buttonText = answerButtons[i].GetComponentInChildren<TextMeshProUGUI>();
-            buttonText.text = question.GetAnswer(i);
+            // string answer = currentQuestion.GetAnswer(i);
+            // Debug.Log($"Answer {i}: {answer}"); // Add this line for debugging
+            buttonText.text = currentQuestion.GetAnswer(i);
         }
     }
 
     void SetButtonState(bool state)
     {
-        for( int i = 0; i < answerButtons.Length; i++) {
+        for (int i = 0; i < answerButtons.Length; i++)
+        {
             Button button = answerButtons[i].GetComponent<Button>();
             button.interactable = state;
         }
@@ -64,8 +120,9 @@ public class Quiz : MonoBehaviour
 
     void SetDefaultButtonSprites()
     {
-        for( int i = 0; i < answerButtons.Length; i++) {
-            Image buttonImage = answerButtons[i].GetComponent<Image>();;
+        for (int i = 0; i < answerButtons.Length; i++)
+        {
+            Image buttonImage = answerButtons[i].GetComponent<Image>();
             buttonImage.sprite = defaultAnswerSprite;
         }
     }
--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -7,32 +7,45 @@ public class Timer : MonoBehaviour
     [SerializeField] float timeToCompleteQuestion = 30f;
     [SerializeField] float timeToShowCorrectAnswer = 10f;
 
-    public bool isAnsweringQuestion = false;
-
+    public bool loadNextQuestion;
+    public float fillFraction;
+    public bool isAnsweringQuestion;
+    
     float timerValue;
 
-    void Update()
+    public void CancelTimer()
     {
-        UpdateTimer();
+        timerValue = 0;
     }
 
-    void UpdateTimer()
+    public void UpdateTimer()
     {
         timerValue -= Time.deltaTime;
 
-        if (isAnsweringQuestion) {
-            if (timerValue <= 0) {
+        if(isAnsweringQuestion)
+        {
+            if(timerValue > 0)
+            {
+                fillFraction = timerValue / timeToCompleteQuestion;
+            }
+            else
+            {
                 isAnsweringQuestion = false;
                 timerValue = timeToShowCorrectAnswer;
             }
-        } else {
-            if (timerValue <=0) {
+        }
+        else
+        {
+            if(timerValue > 0)
+            {
+                fillFraction = timerValue / timeToShowCorrectAnswer;
+            }
+            else
+            {
                 isAnsweringQuestion = true;
                 timerValue = timeToCompleteQuestion;
+                loadNextQuestion = true;
             }
         }
-        
-
-        Debug.Log(timerValue);
     }
 }
